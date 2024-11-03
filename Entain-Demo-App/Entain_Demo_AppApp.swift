857e8d1ef00d53d68ba6820eb18edea315a9c745
Adam Ware <tuppaware@gmail.com>
2024-11-04 09:23:25 +1000

    Initial Commit

---
+++ Entain-Demo-App/Entain_Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Entain_Demo_AppApp.swift
+//  Entain-Demo-App
+//
+//  Created by Adam Ware on 4/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Entain_Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
